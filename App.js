17d761cb79d6b4248195569da40225018f57aa91
Fernando <fernandoclaros@ingeniosolido.com>
2022-07-23 19:09:21 -0500

    start proyect

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Iniciando proyecto</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
